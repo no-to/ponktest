1108a483994fe4c5cacd7319102aa76f53ba3622
no-to <kiriaizanai39@outlook.jp>
2017-05-22 17:06:36 +0900

    Update udon_list2.php

--- udon_list2.php
+++ udon_list2.php
@@ -1,7 +1,6 @@
 <table border="1">
 <tr><th>名前</th><th>価格</th><th>操作</th></tr>
 <?php
-    //一覧
   $pdo = new PDO("mysql:dbname=men", "root","vagrant");
   $st = $pdo->query("SELECT * FROM udon");
   while ($row = $st->fetch()) {
