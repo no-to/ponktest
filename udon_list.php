63edfdaa088a6b5ca2e48aca052e575d4cdab495
no-to <kiriaizanai39@outlook.jp>
2017-05-22 09:57:32 +0200

    udon_list.phpに一覧コメント

--- udon_list.php
+++ udon_list.php
@@ -1,6 +1,7 @@
 <table border="1">
 <tr><th>名前</th><th>価格</th></tr>
 <?php
+    //一覧表示
   $pdo = new PDO("mysql:dbname=men", "root","vagrant");
   $st = $pdo->query("SELECT * FROM udon");
   while ($row = $st->fetch()) {
