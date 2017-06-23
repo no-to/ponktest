9764cd28d6f50852eb1bdaf7b1ee12295027caab
no-to <kiriaizanai39@outlook.jp>
2017-06-23 08:02:39 +0200

    master1

--- udon_insert.php
+++ udon_insert.php
@@ -1,7 +1,8 @@
 <?php
-  $pdo = new PDO("mysql:dbname=men", "root","vagrant");
+  $pdo = new PDO("mysql:dbname=", "","");
   $st = $pdo->prepare("INSERT INTO udon VALUES(?,?)");
   $st->execute(array($_POST['name'], $_POST['price']));
 ?>
 レコードを追加しました。
 aaaaa
+master push
